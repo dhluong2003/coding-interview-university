90f49965d9a906dd5ee84e8671b17fb7e9875b1f
dhluong2003 <dhluong2003@gmail.com>
2025-10-06 23:03:59 +0700

    Marked personal progress

--- code/array.cpp
+++ code/array.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstdint>
 
 class Vector
 {
@@ -8,9 +9,20 @@ class Vector
     int capacity;
 
     Vector(int vecCapacity)
-        :size{0},capacity{16 ? vecCapacity <= 16 : } // round up to next power of 2 if vecCapacity > 16
+        :size{0},capacity{16 ? vecCapacity <= 16 : next_pow_of_2(vecCapacity)} // round up to next power of 2 if vecCapacity > 16
     {
         data = new int[capacity];
+    }
+
+    uint64_t next_pow_of_2(uint64_t t){
+        t--;
+        t |= t >> 1;
+        t |= t >> 2;
+        t |= t >> 4;
+        t |= t >> 8;
+        t |= t >> 16;
+        t++;
+    return t;
     }
 };
 
