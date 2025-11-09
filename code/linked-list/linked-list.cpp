72dd95797cd8a7b540b6d4dc5d7f3bd1a083e8bc
dhluong2003 <dhluong2003@gmail.com>
2025-11-09 22:58:05 +0700

    Marked personal progress

---
+++ code/linked-list/linked-list.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdexcept>
+
+using namespace std;
+
+// using doubly linked list - 2 pointers
+class LinkedList { 
+    int* previous;
+    int data;
+    int* next;
+
+    LinkedList
+};
